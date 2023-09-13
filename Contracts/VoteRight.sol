2b000a0945fabc5f603cd21e73102a2110a2ed2a
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-09-13 22:04:49 +0100

    Update VoteRight.sol
    A skeletal frame work of the Electoral College Federal System (Built especially after Nigeria as the perfect african model to work with). God knows they need it most. [No part of this app or code should be misinterpreted to represent personal values or opinions. I'm just building, guys. No affliations. All pictures are sourceds from wikipedia. Take it up with them]

--- Contracts/VoteRight.sol
+++ Contracts/VoteRight.sol
@@ -20,23 +20,42 @@ contract VoteRight is Ownable, ReentrancyGuard {
 
     struct Candidate {
         string name;
-        uint256[] voteCounts; // One for each electorate
+        uint256 votes;
     }
 
-    //Arrays
-    Candidate[] public candidates;
+    struct Electorate {
+        string name;
+        Candidate[] candidates;
+    }
 
-    //Mappings
+    struct CollatedCandidate {
+        string name;
+        uint256 totalVotes;
+    }
+
+    struct CollatedElectorate {
+        string name;
+        uint256 totalVotes;
+    }
+
+    // Arrays
+    Candidate[] public candidates;
+    Electorate[] public electorates;
+    CollatedCandidate[] public collatedCandidates;
+    CollatedElectorate[] public collatedElectorates;
+
+
+    // Mappings
     mapping(address => uint256) public entryMap;
     mapping(address => bool) public isAdmin;
     mapping(address => bool) public hasVoted;
-    mapping(address => bytes32) private voterHashes; // Maps Merkle root hashes for voters
-    mapping(address => bool) public isRegistered; // Maps Telephone numbers to registration status
+    mapping(address => bytes32) private voterHashes;
+    mapping(address => bool) public isRegistered;
 
-    bytes32[] public merkleProofs; // Merkle root hashes for voter registration
+    bytes32[] public merkleProofs;
 
     event ElectionStarted();
-    event ElectionEnded(uint256 winningCandidateIndex, uint256 winningElectorate);
+    event ElectionEnded(uint256 indexed totalVotes);
     event Voted(address indexed voter, uint256 electorate, uint256 candidateIndex);
 
     constructor() {
@@ -47,10 +66,10 @@ contract VoteRight is Ownable, ReentrancyGuard {
         require(isAdmin[msg.sender], "Not Authorized");
         _;
     }
-    
-    modifier onlyAfterTimelock {            
-    require(entryMap[msg.sender] + timeLock < block.timestamp, "Timelocked.");
-    _;
+
+    modifier onlyAfterTimelock {
+        require(entryMap[msg.sender] + timeLock < block.timestamp, "Timelocked.");
+        _;
     }
 
     modifier onlyVoter() {
@@ -72,10 +91,10 @@ contract VoteRight is Ownable, ReentrancyGuard {
         require(_admin != owner(), "Cannot remove the contract admin");
         isAdmin[_admin] = false;
     }
-    
+
     function addCandidate(string memory _name) public onlyAdmin {
         require(electionState == ElectionState.Active, "Election is not active");
-        
+        candidates.push(Candidate(_name, 0));
     }
 
     function startElection() public onlyAdmin {
@@ -87,43 +106,76 @@ contract VoteRight is Ownable, ReentrancyGuard {
     function endElection() public onlyAdmin {
         require(electionState == ElectionState.Active, "Election is not active");
         electionState = ElectionState.Ended;
-        uint256 winningCandidateIndex;
-        uint256 winningElectorate;
+        collate();
+        uint256 TotalNumberOfVoters;
         // uint256 winningCandidateIndex = getWinningCandidate();
-        emit ElectionEnded(winningCandidateIndex, winningElectorate);
+        emit ElectionEnded(TotalNumberOfVoters);
     }
 
-    function getWinningCandidate() private view returns (uint256, uint256) {
-        require(electionState == ElectionState.Ended, "Election has not ended yet");
-        uint256 maxVotes = 0;
-        uint256 winningIndex = 0;
-        uint256 winningElectorate = 0;
-        // collate all votes from all Electorates for the Electoral College
-        
-        return (winningIndex, winningElectorate);
-    }
-
-    function vote(uint256 _candidateIndex,uint256 _telephone, uint256 _electorate, bytes32[] memory _proof) 
-    public onlyVoter onlyAfterTimelock nonReentrant {
+    function vote(uint256 _telephone, uint256 _electorate, uint256 _candidateIndex, bytes32[] memory _proof)
+        public
+        onlyVoter
+        onlyAfterTimelock
+        nonReentrant
+    {
         require(!paused, "Election is paused.");
-        require(electionState == ElectionState.Active, "Election is not active");       
+        require(electionState == ElectionState.Active, "Election is not active");
         require(!hasVoted[msg.sender], "Voter is already registered");
         bytes32 leaf = keccak256(abi.encodePacked(_telephone)); // Still undecided over telephone or evm address? or both.
         bytes32 merkleRoot = voterHashes[msg.sender];
-        require(MerkleProof.verify(_proof, merkleRoot, leaf), "Invalid Merkle proof"); 
+        require(MerkleProof.verify(_proof, merkleRoot, leaf), "Invalid Merkle proof");
         require(_candidateIndex < candidates.length, "Invalid candidate index");
         require(_electorate < totalElectorates, "Invalid electorate");
-        
-        candidates[_candidateIndex].voteCounts[_electorate]++;
-        hasVoted[msg.sender] = true;
+
+        // Update the candidate's vote count in the specified Electorate
+        electorates[_electorate].candidates[_candidateIndex].votes += 1;
+
+        // Update the total votes for the candidate
+        candidates[_candidateIndex].votes += 1;
+
+        // Emit the Voted event with the electorate and candidate index
         emit Voted(msg.sender, _electorate, _candidateIndex);
+
+        // Mark the voter as having voted
+        hasVoted[msg.sender] = true;
     }
 
+    uint256 public totalVoters; // State variable to store the total number of voters
+
+    function collate() internal {
+        require(electionState == ElectionState.Ended, "Election has not ended yet");
+
+        // Collate votes for candidates
+        for (uint256 i = 0; i < candidates.length; i++) {
+            uint256 totalVotes = 0;
+            for (uint256 j = 0; j < electorates.length; j++) {
+                totalVotes += electorates[j].candidates[i].votes;
+            }
+            collatedCandidates.push(CollatedCandidate(candidates[i].name, totalVotes));
+        }
+
+        // Collate votes for electorates
+        for (uint256 i = 0; i < electorates.length; i++) {
+            uint256 totalSumVotes = 0;
+            for (uint256 j = 0; j < candidates.length; j++) {
+                totalSumVotes += electorates[i].candidates[j].votes;
+            }
+            collatedElectorates.push(CollatedElectorate(electorates[i].name, totalSumVotes));
+        totalVoters = totalSumVotes; // Store the total number of voters
+        }
+    }
+
+    // Getter function to access the total number of voters
+    function getTotalVoters() public view returns (uint256) {
+        return totalVoters;
+    }
+
+    
     function registerVoter(bytes32 _voterHash) public onlyAdmin {
         require(electionState == ElectionState.Inactive, "Registration is closed");
         require(!isRegistered[msg.sender], "Voter is already registered");
         isRegistered[msg.sender] = true;
-        //Allow the public push their registration fingerprints
+        // Allow the public to push their registration fingerprints
         voterHashes[msg.sender] = _voterHash;
     }
 
